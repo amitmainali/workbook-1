4d289104553fedbe87a03e991f12277c099c2ce3
Amit Mainali <amitmainali4@gmail.com>
2025-04-15 11:54:29 -0400

    Methods added

---
+++ Methods/src/main/java/com/pluralsight/methods.java
@@ -0,0 +1,30 @@
+package com.pluralsight;
+
+public class methods {
+
+    public static void main(String[] args) {
+        brushTeeth();
+
+        String watchLater = favoriteShow("Severance");
+        System.out.println(watchLater);
+        int sum = addNums(7,9);
+        System.out.println(sum);
+    }
+
+
+
+    public static void brushTeeth() {
+        System.out.println("1. Wash the toothbrush.");
+        System.out.println("2. Apply toothpaste.");
+        System.out.println("3. Get scrubbing!");
+    }
+
+    public static String favoriteShow(String favShow) {
+        return favShow;
+    }
+
+    public static int addNums(int x, int y) {
+        return x + y;
+    }
+
+}
